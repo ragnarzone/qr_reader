fa51c798cd80a7c31a511b08560129d3746a7783
Dmitrii <ragnarzone@gmail.com>
2023-01-05 11:09:55 +0100

    added usb to serial communication protocol

---
+++ usb_to_rs232.cpp
@@ -0,0 +1,94 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // open port
+    int USB = open( "/dev/ttyUSB0", O_RDWR| O_NOCTTY );
+
+    // set parameters of port //
+
+    struct termios tty;
+    struct termios tty_old;
+    memset (&tty, 0, sizeof tty);
+
+    /* Error Handling */
+    if ( tcgetattr ( USB, &tty ) != 0 ) {
+    std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
+    }
+
+    /* Save old tty parameters */
+    tty_old = tty;
+
+    /* Set Baud Rate */
+    cfsetospeed (&tty, (speed_t)B9600);
+    cfsetispeed (&tty, (speed_t)B9600);
+
+    /* Setting other Port Stuff */
+    tty.c_cflag     &=  ~PARENB;            // Make 8n1
+    tty.c_cflag     &=  ~CSTOPB;
+    tty.c_cflag     &=  ~CSIZE;
+    tty.c_cflag     |=  CS8;
+
+    tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+    tty.c_cc[VMIN]   =  1;                  // read doesn't block
+    tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+    tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+    /* Make raw */
+    cfmakeraw(&tty);
+
+    /* Flush Port, then applies attributes */
+    tcflush( USB, TCIFLUSH );
+    if ( tcsetattr ( USB, TCSANOW, &tty ) != 0) {
+        std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+    }
+
+    // end of parameters setting //
+
+
+
+    // write into port //
+    unsigned char cmd[] = "My_char_command \r";
+    int n_written = 0,
+        spot = 0;
+
+    do {
+        n_written = write( USB, &cmd[spot], 1 );
+        spot += n_written;
+    } while (cmd[spot-1] != '\r' && n_written > 0);
+
+
+
+    // read from the port //
+    int n = 0;
+    spot = 0;
+    char buf = '\0';
+
+    /* Whole response*/
+    char response[1024];
+    memset(response, '\0', sizeof response);
+
+    do {
+        n = read( USB, &buf, 1 );
+        sprintf( &response[spot], "%c", buf );
+        spot += n;
+    } while( buf != '\r' && n > 0);
+
+    if (n < 0) {
+        std::cout << "Error reading: " << strerror(errno) << std::endl;
+    }
+    else if (n == 0) {
+        std::cout << "Read nothing!" << std::endl;
+    }
+    else {
+        std::cout << "Response: " << response << std::endl;
+    }
+
+    return 0;
+}
