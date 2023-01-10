a0205a7309fa4e6585d587c36b2737a9fcac1ed3
Dmitrii <ragnarzone@gmail.com>
2023-01-10 10:12:43 +0100

    added front representation of captured qr picture and incorporated link

--- cv.py
+++ cv.py
@@ -1,4 +1,7 @@
 import cv2
+from jinja2 import Environment, FileSystemLoader
+
+# read qr
 
 cam = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
@@ -17,3 +20,13 @@ cv2.imwrite('/var/www/html/qr_reader/qr_pic.jpg', image)
 print(a)
 cam.release()
 cv2.destroyAllWindows()
+
+# write template
+
+environment = Environment(loader=FileSystemLoader("./"))
+template = environment.get_template("qr_template.txt")
+
+filename = "qr.html"
+content = template.render(link=a)
+html = open(filename, mode="w", encoding="utf-8")
+html.write(content)
