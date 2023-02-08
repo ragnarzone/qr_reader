20c8dabb99defd416f593c5478f496d06f13fd99
Dmitrii <ragnarzone@gmail.com>
2023-02-08 12:08:17 +0100

    added script for triggering camera reading

--- tested_code/write_xml.py
+++ tested_code/write_xml.py
@@ -1,32 +1,35 @@
+#!/usr/bin/python3
 from datetime import datetime
 import requests
 
 
 #http://hutiss0stgweb81/Linecontrol/camxhandler.ashx
 
+
+
 def send_message():
     url = 'http://hutiss0stgweb81/Linecontrol/camxhandler.ashx'
-    xml = open("message.xml", "r")
-    x = xml.readlines()
-    #print(x)
-    response = requests.post(url, xml)
-    #print(response.text)
+    xml = open("/var/www/html/qr_reader/tested_code/message.xml", "r")
+    headers = {'Content-Type':'text/xml'}
+    response = requests.post(url, xml, headers)
+    print(response.text)
 
 
 def generate_message(itemInstanceId):
     now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.+01:00")
     itemInstanceId=itemInstanceId
-    laneId="some string"
-    zoneId="some string"
-    scannerId="some string"
+    laneId="1"
+    zoneId="1"
+    scannerId="1234"
 
-    contentLength = "0"
+    contentLength = "959"
     sender = "C120"
+    actor = "C120"
 
-    file1 = open('/var/www/html/qr_reader/message.xml', 'w')
+    file1 = open('/var/www/html/qr_reader/tested_code/message.xml', 'w')
 
     toWrite = [
-        "---BOUNDARY-\n",
+        "\n---BOUNDARY-\n",
         "Content-Type: text/xml\n",
         "Content-Transfer-Encoding: 8bit\n",
         "Content-ID: <example:soap:MaRC>\n"
@@ -35,10 +38,10 @@ def generate_message(itemInstanceId):
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
         "<soap-env:Envelope xmlns:soap-env=\"http://schemas.xmlsoap.org/soap/envelope/\">\n",
         "<soap-env:Header>\n",
-        "<ipc2502mh:MessageInfo\n xmlns:ipc2501mh=\"http://webstds.ipc.org/2501/MessageInfo.xsd\"\n " +
-        "soap-env:actor=\"Jabil.TIS.Broker13\"\n" + 
+        "<ipc2501mh:MessageInfo\n xmlns:ipc2501mh=\"http://webstds.ipc.org/2501/MessageInfo.xsd\"\n " +
+        "soap-env:actor=\"" + actor +"\"\n" + 
         " sender=\"" + sender + "\" \n" +
-        " destination=\"Jabil.TIS.Broker13\" \n" +
+        " destination=\"Jabil.TIS.LineControl\" \n" +
         " dateTime=\"" + now + "\"\n" + 
         " messageSchema=\"http://webstds.ipc.org/2541/ItemIdentifierRead.xsd\"\n" +
         " messageId=\"CAMX-API Georgia Tech" + now + "\"\n" +  
@@ -54,19 +57,17 @@ def generate_message(itemInstanceId):
         "\n",
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
         "<ItemIdentifierRead\n", 
-        "\tdateTime=\"" + now + "\"\n",
-        "\titemInstanceId=\"" + itemInstanceId + "\"\n",
-        "\tlaneId=\"" + laneId + "\"\n",
-        "\tzoneId=\"" + zoneId + "\"\n",
-        "\tscannerId=\"" + scannerId + "\"\n",
+        "dateTime=\"" + now + "\"\n",
+        "scannerId=\"" + scannerId + "\"\n",
+        "itemInstanceId=\"" + itemInstanceId + "\"\n",
+        "laneId=\"" + laneId + "\"\n",
+        "zoneId=\"" + zoneId + "\"\n",
         "/>\n",
-        "---BOUNDARY---"
+        "---BOUNDARY---\n"
     ]
   
     file1.writelines(toWrite)
     file1.close()
 
-generate_message("12345")
+generate_message("1234")
 send_message()
-
-
